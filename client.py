49099381653497ce0020411d1200448b58ee2fe0
nyanta <156566971+nyantarou2001002@users.noreply.github.com>
2024-09-17 21:52:26 +0900

    Update client.py

--- client.py
+++ client.py
@@ -1,38 +1,22 @@
 import socket
-import threading
 
-# サーバーのホストとポートを設定
-HOST = 'localhost'
-PORT = 12345
-BUFFER_SIZE = 4096
-
-# サーバーからのメッセージを受信する関数
-def receive_messages():
-    while True:
-        try:
-            message, _ = client_socket.recvfrom(BUFFER_SIZE)
-            print(message.decode('utf-8'))
-        except:
-            print("メッセージの受信中にエラーが発生しました。")
-            break
-
-# クライアントのセットアップ
-def start_client():
-    username = input("ユーザー名を入力してください: ")
-    while True:
-        message = input("メッセージを入力してください: ")
-        if message:
-            # メッセージのフォーマット: [ユーザー名長さ (1バイト)] + [ユーザー名] + [メッセージ]
-            username_len = len(username.encode('utf-8'))
-            formatted_message = bytes([username_len]) + username.encode('utf-8') + message.encode('utf-8')
-            client_socket.sendto(formatted_message, (HOST, PORT))
-
-# クライアントソケットの作成
+# サーバの設定
+server_ip = '127.0.0.1'
+server_port = 12345
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# メッセージ受信用のスレッドを開始
-receive_thread = threading.Thread(target=receive_messages)
-receive_thread.start()
+# ユーザー名の入力
+username = input("ユーザー名を入力してください: ")
 
-# クライアントを起動してサーバに接続
-start_client()
+while True:
+    # メッセージの入力
+    message = input("メッセージを入力してください: ")
+
+    # ユーザー名の長さを1バイトで表現し、ユーザー名とメッセージを送信
+    usernamelen = len(username.encode('utf-8'))
+    data = usernamelen.to_bytes(1, 'big') + username.encode('utf-8') + message.encode('utf-8')
+    client_socket.sendto(data, (server_ip, server_port))
+
+    # サーバからの応答を受信
+    response, _ = client_socket.recvfrom(4096)
+    print(response.decode('utf-8'))
