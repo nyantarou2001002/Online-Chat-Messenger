ebc4e759c7797c4ff5fc955f2f1d4cf62a51c6c3
Ikumo Takahashi <ikumo1118free@gmail.com>
2024-09-22 14:37:58 +0900

    Added tcp-1

--- client.py
+++ client.py
@@ -1,11 +1,21 @@
 import socket
 import threading
 
-def listen_for_messages(client_socket):
+def protocol_header(room_name, operation, state, payload):
+    if len(payload.encode("utf-8")) < 29:
+        payload = payload.ljust(29, " ")
+
+    return len(room_name).to_bytes(1, 'big') +\
+        operation.to_bytes(1, 'big') +\
+        state.to_bytes(1, 'big') +\
+        payload.encode('utf-8')
+        
+
+def listen_for_messages(udp_socket):
     while True:
         try:
             # サーバからのメッセージを受信
-            data, _ = client_socket.recvfrom(4096)
+            data, _ = udp_socket.recvfrom(4096)
             print(data.decode('utf-8'))
         except Exception as e:
             print(f"エラーが発生しました: {e}")
@@ -14,13 +24,38 @@ def listen_for_messages(client_socket):
 # クライアントの設定
 server_ip = '127.0.0.1'
 server_port = 12345
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+client_ip = 'localhost'
+client_port = 54321
+
+# サーバーとの接続
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# サーバーとの接続を保証
+try:
+    tcp_socket.connect((server_ip, server_port))
+except socket.error as err:
+    print(err)
+
+# ユーザー名を取得
 username = input("ユーザー名を入力してください: ").strip()
-print("ユーザー名の入力を受け付けました。会話をお楽しみください。")
+# オペレーションを取得
+operation = int(input('１又は２を選択してください (1:ルーム作成 2:ルームに参加): '))
+# オペレーションを元にルーム名を取得
+if(operation == 1):
+    roomname = input('ルーム名を入力してください: ').strip()
+else:
+    roomname = ''
+# 初期の操作コードは０
+state = 0
+
+# サーバーにヘッダーを送信
+tcp_socket.send(protocol_header(roomname, operation, state, username))
+print("ユーザーの入力を受け付けました。会話をお楽しみください。")
 
 # サーバからのメッセージを別スレッドで受信
-listen_thread = threading.Thread(target=listen_for_messages, args=(client_socket,))
+listen_thread = threading.Thread(target=listen_for_messages, args=(udp_socket,))
 listen_thread.daemon = True  # メインスレッドが終了するときに自動的に終了する
 listen_thread.start()
 
@@ -33,7 +68,7 @@ try:
         
         formatted_message = f"{username} : {message}"
         message_data = formatted_message.encode('utf-8')
-        client_socket.sendto(message_data, (server_ip, server_port))
+        udp_socket.sendto(message_data, (server_ip, server_port))
 
 except Exception as e:
     print(f"エラーが発生しました: {e}")
--- server.py
+++ server.py
@@ -5,25 +5,47 @@ import time
 # サーバの設定
 server_ip = '127.0.0.1'
 server_port = 12345
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((server_ip, server_port))
+
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 clients = {}
 timeout_seconds = 60  # 60秒間メッセージがなければクライアントを削除
 
+udp_socket.bind((server_ip, server_port))
+tcp_socket.bind((server_ip, server_port))
 print("サーバが起動しました...")
 
 while True:
     try:
+        tcp_socket.listen()
+        connection, client_address = tcp_socket.accept()
+
+        # クライアントかヘッダーを受信する
+        header = connection.recv(32)
+        
+        room_name_size = int.from_bytes(header[:1], "big")
+        operation = int.from_bytes(header[1:2], "big")
+        state = int.from_bytes(header[2:3], "big")
+        payload_size = int.from_bytes(header[3:32], "big")
+
+        print("------------------------------")
+        print("Recive header from client.")
+        print("room_name_size: ", room_name_size)
+        print("operation: ", operation)
+        print("state: ", state)
+        print("payload_size: ", payload_size)
+        print("------------------------------")
+
         # 現在時刻を取得
         current_time = time.time()
 
         # 受信準備 (サーバソケットでデータが来るのを待つ)
-        ready_sockets, _, _ = select.select([server_socket], [], [], 1)
+        ready_sockets, _, _ = select.select([tcp_socket], [], [], 1)
 
         if ready_sockets:
             # メッセージ受信
-            data, addr = server_socket.recvfrom(4096)
+            data, addr = tcp_socket.recvfrom(4096)
 
             # クライアントの最終メッセージ送信時刻を更新
             clients[addr] = current_time
@@ -31,7 +53,7 @@ while True:
             # 他の全クライアントに送信
             for client in list(clients.keys()):
                 if client != addr:  # 自分には送信しない
-                    server_socket.sendto(data, client)
+                    tcp_socket.sendto(data, client)
 
         # 古いクライアントを削除（タイムアウトしたクライアント）
         clients = {addr: last_time for addr, last_time in clients.items() if current_time - last_time < timeout_seconds}
